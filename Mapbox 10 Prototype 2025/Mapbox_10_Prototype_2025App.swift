56d2652e62a85345263634b8964d10665bf68e42
Jim Margolis <57914060+JimMargolis@users.noreply.github.com>
2024-11-22 15:00:20 -0700

    Initial Commit

---
+++ Mapbox 10 Prototype 2025/Mapbox_10_Prototype_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Mapbox_10_Prototype_2025App.swift
+//  Mapbox 10 Prototype 2025
+//
+//  Created by Jim Margolis on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Mapbox_10_Prototype_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
