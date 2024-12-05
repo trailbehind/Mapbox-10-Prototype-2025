fb8c07162694167b47243cc84fc6411c1393d283
Jim Margolis <57914060+JimMargolis@users.noreply.github.com>
2024-12-05 15:49:56 -0700

    random images

--- Mapbox 10 Prototype 2025/WaypointManager.swift
+++ Mapbox 10 Prototype 2025/WaypointManager.swift
@@ -11,21 +11,23 @@ struct Waypoint {
     var id: Int
     var latitude: Double
     var longitude: Double
+    var image: String
     
     static func waypointToFeature(waypoint: Waypoint) -> Feature {
-        // Convert Waypoint latitude and longitude to CLLocationCoordinate2D
         let coordinate = CLLocationCoordinate2D(latitude: waypoint.latitude, longitude: waypoint.longitude)
-        
-        // Initialize and return the Feature object using the coordinate
         let point = Point(coordinate)
         var feature = Feature(geometry: .point(point))
-        feature.properties = ["id": .string(String(waypoint.id))]
+        feature.properties = [
+                "id": .string(String(waypoint.id)),
+                "icon": .string(waypoint.image)
+            ]
         return feature
     }
 }
 
 class WaypointManager {
     static let shared = WaypointManager()
+    private let defaultImageName = "pin"
 
     var waypoints: [Waypoint] = []
     
@@ -49,7 +51,7 @@ class WaypointManager {
                     let latitude = startLatitude + (Double(row) * spacing)
                     let longitude = startLongitude + (Double(col) * spacing)
                     
-                    let waypoint = Waypoint(id: row * gridDimension + col, latitude: latitude, longitude: longitude)
+                    let waypoint = Waypoint(id: row * gridDimension + col, latitude: latitude, longitude: longitude, image: randomMarkerImage())
                     generatedWaypoints.append(waypoint)
                 }
             }
@@ -57,7 +59,7 @@ class WaypointManager {
             for i in 0..<totalPoints {
                 let latitude = startLatitude + Double.random(in: -spacing...spacing)
                 let longitude = startLongitude + Double.random(in: -spacing...spacing)
-                let waypoint = Waypoint(id: i, latitude: latitude, longitude: longitude)
+                let waypoint = Waypoint(id: i, latitude: latitude, longitude: longitude, image: randomMarkerImage())
                 generatedWaypoints.append(waypoint)
             }
         }
@@ -69,29 +71,17 @@ class WaypointManager {
     func addWaypointsLayer(mapView: MapboxMaps.MapView) {
         let sourceId = "waypoints"
         let layerId = "waypoint-layer"
-        let waypointImageName = "pin" // Name of the image asset
 
-        // Load the image asset
-        if let waypointImage = UIImage(named: waypointImageName) {
-            do {
-                if !mapView.mapboxMap.style.imageExists(withId: waypointImageName) {
-                    try mapView.mapboxMap.style.addImage(waypointImage, id: waypointImageName)
-                }
-            } catch {
-                print("Failed to add image to style: \(error)")
-            }
-        } else {
-            print("Image \(waypointImageName) not found.")
-        }
+        loadImageAssets(mapView: mapView)
 
         let options = CustomGeometrySourceOptions(
             fetchTileFunction: { tileID in
                 do {
-                    let visibleWaypoints = WaypointDataSource.shared.getWaypointsForTile(tileID: tileID)
+                    let waypoints = WaypointDataSource.shared.getWaypointsForTile(tileID: tileID)
                     try mapView.mapboxMap.style.setCustomGeometrySourceTileData(
                         forSourceId: sourceId,
                         tileId: tileID,
-                        features: visibleWaypoints
+                        features: waypoints
                     )
                 } catch {
                     print("Error setting custom geometry source tile data: \(error)")
@@ -102,27 +92,55 @@ class WaypointManager {
         )
 
         do {
-            try mapView.mapboxMap.style.addCustomGeometrySource(withId: sourceId, options: options)
-
-            var symbolLayer = SymbolLayer(id: layerId)
-            symbolLayer.source = sourceId
-            symbolLayer.iconImage = .constant(.name(waypointImageName))
-            //jmTODO: icon overlap not working, do we need clustering logic
-            symbolLayer.iconAllowOverlap = .constant(false)
-            symbolLayer.iconSize = .constant(1.0)
-            symbolLayer.iconAnchor = .constant(.center)
-            symbolLayer.iconOffset = .constant([0, 0])
-
-            // Use the "id" property in the textField
-            symbolLayer.textField = .constant("{id}")
-            symbolLayer.textSize = .constant(12.0)
-            symbolLayer.textColor = .constant(.init(.black))
-            symbolLayer.textOffset = .constant([0, 2])
-
-            try mapView.mapboxMap.style.addLayer(symbolLayer)
+            if !mapView.mapboxMap.style.sourceExists(withId: sourceId) {
+                try mapView.mapboxMap.style.addCustomGeometrySource(withId: sourceId, options: options)
+                
+                var symbolLayer = SymbolLayer(id: layerId)
+                symbolLayer.source = sourceId
+                symbolLayer.iconImage = .expression(Exp(.get) { "icon" })
+                //jmTODO: icon overlap not working, do we need clustering logic
+                symbolLayer.iconAllowOverlap = .constant(false)
+                symbolLayer.iconSize = .constant(1.0)
+                symbolLayer.iconAnchor = .constant(.center)
+                symbolLayer.iconOffset = .constant([0, 0])
+                symbolLayer.textField = .constant("{id}")
+                symbolLayer.textSize = .constant(12.0)
+                symbolLayer.textColor = .constant(.init(.black))
+                symbolLayer.textOffset = .constant([0, 2])
+                
+                try mapView.mapboxMap.style.addLayer(symbolLayer)
+            }
 
         } catch {
             print("Error adding waypoints or symbol layer: \(error)")
+        }
+    }
+    
+    private func randomMarkerImage() -> String {
+        return MarkerDecoration.allCases.randomElement()?.rawValue ?? defaultImageName
+    }
+    
+    private func loadImageAssets(mapView: MapboxMaps.MapView) {
+        let startTime = Date()
+        
+        MarkerDecoration.allCases.forEach { loadImage($0.rawValue, mapView: mapView) }
+        
+        let elapsedTime = Date().timeIntervalSince(startTime)
+        print("Loaded all image assets in \(elapsedTime) seconds.")
+    }
+
+    
+    private func loadImage(_ name: String, mapView: MapboxMaps.MapView) {
+        if let waypointImage = UIImage(named: name) {
+            do {
+                if !mapView.mapboxMap.style.imageExists(withId: name) {
+                    try mapView.mapboxMap.style.addImage(waypointImage, id: name)
+                }
+            } catch {
+                print("Failed to add image to style: \(error)")
+            }
+        } else {
+            print("Image \(name) not found.")
         }
     }
 }
@@ -140,12 +158,7 @@ class WaypointDataSource {
             //        let tileOptions = TileOptions(tolerance: 0.375, tileSize: 256, buffer: 1, clip: true, wrap: false)
             tileBounds = Math.bufferBounds(bounds: tileBounds, buffer: 1 / 256)
             if tileBounds.contains(latitude: waypoint.latitude, longitude: waypoint.longitude) {
-                let coordinate = CLLocationCoordinate2D(latitude: waypoint.latitude, longitude: waypoint.longitude)
-                let point = Point(coordinate)
-                var feature = Feature(geometry: .point(point))
-                let idAsString = String(waypoint.id)
-                feature.properties = ["id": .string(idAsString)]
-                return feature
+                return Waypoint.waypointToFeature(waypoint: waypoint)
             } else {
                 return nil
             }
